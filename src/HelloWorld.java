a5ebd95eb41b334acb18bf70ac56ab444392a761
Philipp Mödritscher <philippmo@edu.aau.at>
2018-06-06 12:53:14 +0200

    Send html as first Page...(FileNotFound)

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -6,6 +6,10 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -14,11 +18,12 @@ import java.util.Date;
 public class HelloWorld {
     // The Java method will process HTTP GET requests
     @GET
-    // The Java method will produce content identified by the MIME Media type "text/plain"
-    @Produces("text/plain")
-    public String getClichedMessage() {
-        // Return some cliched textual content
-        return "Hotel Room Manager entry point.";
+    @Produces({MediaType.TEXT_HTML})
+    public InputStream viewHome()throws FileNotFoundException
+    {
+        File f = new File("test.html");
+        return new FileInputStream(f);
+
     }
 
     @GET
