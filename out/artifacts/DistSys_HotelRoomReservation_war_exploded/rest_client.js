0b1a147e26c09f5f0f074e936faf97dddc7909ac
Anja Ressmann <anjress@gmail.com>
2018-06-16 01:04:06 +0200

    front end fings

--- out/artifacts/DistSys_HotelRoomReservation_war_exploded/rest_client.js
+++ out/artifacts/DistSys_HotelRoomReservation_war_exploded/rest_client.js
@@ -4,11 +4,6 @@
 
 // The root URL for the RESTful services
 var rootURL = "http://localhost:8080/DistSys_HotelRoom_war_exploded/";
-var arrivalDate;
-var departureDate;
-var roomType;
-var givenname;
-var surname;
 
 //listRooms() ++++++  GET /hotelrooms - list of all hotelrooms 
 
@@ -21,106 +16,19 @@ var surname;
 //setRoomType()   ++  POST /roomtypes - create/update a roomtype uses communication.AdminRequest (password is currently "admin"), eg:
 //book() 	  ++++++  POST /roomtypes/booking
 
-$( document ).ready(function() {
-    document.getElementById("arrivalDate").valueAsDate = new Date();
-    document.getElementById("departureDate").valueAsDate = new Date();
-
-   //if i knew how the output looks like I'd like to use it as options in the #roomtype input
-    listRoomTypes();
-});
-
-function checkAvailability() {
-    arrivalDate = document.getElementById('arrivalDate').value;
-    departureDate = document.getElementById('departureDate').value;
-    roomType = document.getElementById('roomType').value;
-
-    console.log(window.location.pathname);
-
-    if(departureDate < arrivalDate) {
-        //invalid
-    } else {
-        $("#reservation-form").addClass('invisible');
-        $("#book-form").removeClass('invisible');
-        $('#book-form').addClass('visible');
-
-        $('#availableText').html('The room of type ' + roomType + " is from " + arrivalDate + " to " + departureDate + " still available!");
-
-
-        //try to post this info on server
-        $.ajax({
-            type: 'GET',
-            url: rootURL + 'check?arrivalDate='+arrivalDate+'&departureDate='+departureDate+'&roomType='+roomType,
-            crossDomain:    true,
-            dataType: "json",
-            success: function(result){
-                console.log(result);
-                //if result 204, show #book-form
-                //else result 200 and json with alternatives
-
-            },
-            error: function(result){
-                console.log(result);
-            }
-        });
-    }
-}
-
-function goBackToForm() {
-    $("#book-form").addClass('invisible');
-    $("#reservation-form").removeClass('invisible');
-    $('#reservation-form').addClass('visible');
-}
-
-function bookRoom() {
-    givenname = document.getElementById('givenname').value;
-    surname = document.getElementById('surname').value;
-    console.log(givenname + " ");
-
-    if(givenname != "" && surname != "") {
-
-        //data as json - although typeId still int
-        var data = JSON.stringify({
-            "typeId": 1,
-            "startDate": arrivalDate,
-            "endDate": departureDate,
-            "firstName":givenname,
-            "lastName": surname
-        });
-
-        //post that shit
-        $.ajax({
-            type: 'POST',
-            contentType: 'application/json',
-            url: rootURL + '/roomtypes/booking',
-            dataType: "json",
-            data: data,
-            success: function (result) {
-                console.log(result);
-            },
-            error: function (result) {
-                console.log(result);
-            }
-        });
-    } else {
-        //not valid
-    }
-}
-
-
-//mödi stuff; some things still useful, some not :D
+//TODO: success ?
 
 function listRooms() {
     $.ajax({
         type: 'GET',
         url: rootURL + 'hotelrooms',
-        crossDomain:    true,
         dataType: "json", // data type of response
         success: function(result){
-            console.log(result + " " + rootURL);
+            console.log(result);
            // alert('Success: ' + renderList);
         },
         error: function(result){
-            console.log(result + " " + rootURL);
+            console.log(result);
            // alert('Error: ' + result);
         }
     });
