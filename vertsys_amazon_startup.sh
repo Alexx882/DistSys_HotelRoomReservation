10d99ec1592aeb1a823790335bf877f72840b9e7
Alexander <allercher@edu.aau.at>
2018-07-03 19:13:37 +0200

    added connection info to hibernate

--- vertsys_amazon_startup.sh
+++ vertsys_amazon_startup.sh
@@ -16,8 +16,15 @@ cp -a ~/DistSys_HotelRoomReservation_Server/out/artifacts/DistSys_HotelRoomReser
 
 # asadmin> start-domain
 # asadmin> deploy DistSys_HotelRoomReservation_Server/out/artifacts/DistSys_HotelRoomReservation_war_exploded
+# asadmin> deploy out/artifacts/DistSys_HotelRoomReservation_war_exploded
 # asadmin> exit
 
-# asadmin> change-admin-password (initial is username:admin, password:[enter])
-# asadmin> enable-secure-admin (use the admin username and password which you just set)
-# asadmin> restart-domain
+# setup database and admin user
+sudo yum install mysql
+sudo yum install mysql-server
+sudo service mysqld start
+mysqladmin -u root password '12345'
+mysql -u root -h localhost -p
+# mysql> CREATE USER 'root'@'%' IDENTIFIED BY '12345';
+# mysql> GRANT ALL PRIVILEGES ON *.* TO 'root'@'%'
+#    -> WITH GRANT OPTION;
