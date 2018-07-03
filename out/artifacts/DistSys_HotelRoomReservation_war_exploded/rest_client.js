7c18a3d51142f2ec8886a2a0e3b6f670f9da6227
Anja Ressmann <anjress@gmail.com>
2018-07-03 22:08:04 +0200

    mörtsch

--- out/artifacts/DistSys_HotelRoomReservation_war_exploded/rest_client.js
+++ out/artifacts/DistSys_HotelRoomReservation_war_exploded/rest_client.js
@@ -217,7 +217,7 @@ function bookRoomCommand() {
 
     if (givenname != "" && surname != "") {
 
-        var data = buildBookingRequest(roomType, arrivalDate, departureDate, givenname, surname);
+        var data = buildBookingRequest(roomType, arrivalDate.toJSON(), departureDate.toJSON(), givenname, surname);
         bookRoomServer(data, function(result){});
 
 
@@ -264,12 +264,12 @@ function saveRoomType(id) {
 
     $('#button-'+id).html(
         "<div class='col-md-6'><button class='borderless-button' id='"+id+"' onClick='editRoomType(this)'>Edit</button></div> " +
-        "<div class='col-md-6'><button class='borderless-button' id='"+id+"-delete' onClick='deleteRoomType(this)'>Delete " +
+        "<div class='col-md-6'><button class='borderless-button' id='"+id+"-delete' onClick='deleteRoomTypeCommand(this)'>Delete " +
         "</button></div> " )
 
 }
 
-function deleteRoomType(button) {
+function deleteRoomTypeCommand(button) {
     var fullId = button.id;
     var id = fullId.split('-');
 
@@ -289,8 +289,6 @@ function deleteRoomType(id) {
         url: rootURL + 'roomtypes/'+id,
         crossDomain: true,
         dataType: "json",
-        data: JSON.stringify({
-        "password": password}),
         success: function(result){
             return true;
         },
@@ -515,7 +513,7 @@ function listExisitingRooms(json) {
             "<div class='col-md-2' id='price-"+id+"'>" + json[n].price + "</div> " +
             "<div class='col-md-3 row' id='button-"+id+"'> " +
             "<div class='col-md-6'><button class='borderless-button' id='"+id+"' onClick='editRoomType(this)'>Edit</button></div> " +
-            "<div class='col-md-6'<button class='borderless-button' id='"+id+"-delete' onClick='deleteRoomType(this)'>Delete " +
+            "<div class='col-md-6'><button class='borderless-button' id='"+id+"-delete' onClick='deleteRoomTypeCommand(this)'>Delete " +
             "</button></div> " +
             "</div> " +
             "</div>");
